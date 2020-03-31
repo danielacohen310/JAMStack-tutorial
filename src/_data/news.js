08d32b17efae6a9b5ca29e332de0af5281628b84
dac284 <dac284@cornell.edu>
2020-03-31 14:51:31 -0400

    descriptive env var2

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
-var url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.API_KEY}&pageSize=5`;
+var url = `http://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`;
 
 module.exports = async function() {
-    try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      console.error(error);
-    }
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
+};
