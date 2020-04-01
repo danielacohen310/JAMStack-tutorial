e472b106c6e83b73d15601f28608a9731c661b73
danielacohen310 <danielacohen310@gmail.com>
2020-04-01 16:04:39 -0400

    fix weather api key

--- src/_functions/weatherapi.js
+++ src/_functions/weatherapi.js
@@ -3,12 +3,12 @@ const axios = require("axios");
 exports.handler = async function(event, context, callback) {
 
   const { lat, lon } = event.queryStringParameters;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}&units=imperial`;
 
   const response = await axios.get(url);
   callback(null, {
     statusCode: 200,
     body: JSON.stringify(response.data)
   });
-  
+
 };
